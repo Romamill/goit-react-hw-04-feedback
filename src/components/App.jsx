aff6be18d4134edcce432a4268c9443f68bdb831
Romamill <122994080+Romamill@users.noreply.github.com>
2023-08-28 10:35:57 +0300

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,82 @@
+
+import React, { Component } from 'react';
+import Statistics from './Statistics/Statistics';
+import FeedbackOptions from './FeedbackOptions/FeedbackOptions';
+import Section from './Section/Section';
+import Notification from './Notification/Notification';
+import styled from 'styled-components';
+
+const AppContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+  background-color: #f0f0f0;
+`;
+
+const ContentContainer = styled.div`
+  text-align: center;
+  padding: 20px;
+  border: 2px solid #ccc;
+  transform: scale(2);
+`;
+
+
+class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  handleLeaveFeedback = (type) => {
+    this.setState((prevState) => ({ [type]: prevState[type] + 1 }));
+  };
+
+  countTotalFeedback = () => {
+    const { good, neutral, bad } = this.state;
+    return good + neutral + bad;
+  };
+
+  countPositiveFeedbackPercentage = () => {
+    const { good } = this.state;
+    const totalFeedback = this.countTotalFeedback();
+    return totalFeedback === 0 ? 0 : Math.round((good / totalFeedback) * 100);
+  };
+
+  render() {
+    const { good, neutral, bad } = this.state;
+    const totalFeedback = this.countTotalFeedback();
+
+    return (
+      <AppContainer>
+        <ContentContainer>
+
+        <Section title="Pleace leave feedback">
+          <FeedbackOptions
+            options={['good', 'neutral', 'bad']}
+            onLeaveFeedback={this.handleLeaveFeedback}
+          />
+        </Section>
+
+        {totalFeedback === 0 ? (
+          <Notification message="There is no feedback" />
+        ) : (
+          <Section title="Statistics">
+            <Statistics
+              good={good}
+              neutral={neutral}
+              bad={bad}
+              total={totalFeedback}
+              positivePercentage={this.countPositiveFeedbackPercentage()}
+            />
+          </Section>
+          )}
+        </ContentContainer>
+      </AppContainer>
+    );
+  }
+}
+
+export default App;
+
